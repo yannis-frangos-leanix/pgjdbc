c8d16c8683f3f204ff069b88ac79a238e60729c1
Yannis Frangos <yannis.frangos@leanix.net>
2024-06-12 14:51:48 +0300

    Facilitate query execution counting per session usage

---
+++ pgjdbc/src/main/java/org/postgresql/core/v3/ContextAwareQueryExecutorImpl.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2016, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.core.v3;
+
+import org.postgresql.core.PGStream;
+import org.postgresql.core.ParameterList;
+import org.postgresql.core.Query;
+import org.postgresql.core.ResultHandler;
+import org.postgresql.jdbc.BatchResultHandler;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ContextAwareQueryExecutorImpl extends QueryExecutorImpl {
+
+  private final Context context = new Context("", 0);
+
+  public ContextAwareQueryExecutorImpl(
+      PGStream pgStream,
+      int cancelSignalTimeout,
+      Properties info)
+      throws SQLException, IOException {
+    super(pgStream, cancelSignalTimeout, info);
+  }
+
+  @Override
+  public void execute(
+      Query[] queries,
+      @Nullable ParameterList[] parameterLists,
+      BatchResultHandler batchHandler,
+      int maxRows,
+      int fetchSize,
+      int flags,
+      boolean adaptiveFetch) throws SQLException {
+    super.execute(queries, parameterLists, batchHandler, maxRows, fetchSize, flags, adaptiveFetch);
+    context.sqlCount += queries.length;
+  }
+
+  @Override
+  public void execute(
+      Query query,
+      @Nullable ParameterList parameters,
+      ResultHandler handler,
+      int maxRows,
+      int fetchSize,
+      int flags,
+      boolean adaptiveFetch) throws SQLException {
+    super.execute(query, parameters, handler, maxRows, fetchSize, flags, adaptiveFetch);
+    context.sqlCount++;
+  }
+
+  public Context getContext() {
+    return context.copy();
+  }
+
+  public void resetContext(String sessionOwnerIdentifier) {
+    context.sessionOwnerIdentifier = sessionOwnerIdentifier;
+    context.sqlCount = 0;
+  }
+
+  public static class Context {
+    private String sessionOwnerIdentifier = "";
+    private int sqlCount;
+
+    public Context(String sessionOwnerIdentifier, int sqlCount) {
+      this.sessionOwnerIdentifier = sessionOwnerIdentifier;
+      this.sqlCount = sqlCount;
+    }
+
+    private Context(Context context) {
+      this.sessionOwnerIdentifier = context.sessionOwnerIdentifier;
+      this.sqlCount = context.sqlCount;
+    }
+
+    public String getSessionOwnerIdentifier() {
+      return sessionOwnerIdentifier;
+    }
+
+    public void setSessionOwnerIdentifier(String sessionOwnerIdentifier) {
+      this.sessionOwnerIdentifier = sessionOwnerIdentifier;
+    }
+
+    public int getSqlCount() {
+      return sqlCount;
+    }
+
+    private Context copy() {
+      return new Context(this);
+    }
+
+  }
+
+}
